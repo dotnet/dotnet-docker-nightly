d9bf72fd14a7e7850d35795a9e80bbc310e75004
Michael Simons (VISUAL STUDIO) <msimons@microsoft.com>
2016-09-20 15:28:49 -0500

    Dump dotnet version

--- build-and-test.ps1
+++ build-and-test.ps1
@@ -10,6 +10,8 @@ $dockerRepo="microsoft/dotnet-nightly"
 
 pushd $PSScriptRoot
 
+docker --version
+
 Get-ChildItem -Recurse -Filter Dockerfile | where DirectoryName -like "*\$OS*" | sort DirectoryName | foreach {
     $tag = "$($dockerRepo):" + $_.DirectoryName.Replace($PSScriptRoot, '').TrimStart('\').Replace('\', '-') -replace "${OS}$", "${OS}-sdk"
     Write-Host "----- Building $tag -----"
