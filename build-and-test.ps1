7e9865f660fa8e7c8d0a0daa7fc46a8588c38e8b
Michael Simons (VISUAL STUDIO) <msimons@microsoft.com>
2016-10-19 14:33:49 -0500

    Debugging

--- build-and-test.ps1
+++ build-and-test.ps1
@@ -7,8 +7,8 @@ pushd $PSScriptRoot
 
 Get-ChildItem -Recurse -Filter Dockerfile | where DirectoryName -like "*\nanoserver*" | sort DirectoryName | foreach {
     $tag = "$($dockerRepo):" + $_.DirectoryName.Replace($PSScriptRoot, '').TrimStart('\').Replace('\', '-') -replace "nanoserver$", "nanoserver-sdk"
-    Write-Host "----- Building $tag -----"
-    docker build --no-cache -t $tag $_.DirectoryName
+    Write-Host "----- Building $tag -----$DirectoryName "
+    #docker build --no-cache -t $tag $_.DirectoryName
     if (-NOT $?) {
         throw "Failed building $tag"
     }
