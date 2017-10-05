d3a65f326a1cc4b4585e9c846f342a15c1c1ce81
Ravi Eda <raeda@microsoft.com>
2017-10-05 16:08:47 -0500

    Workaround for asterix in job name.

--- netci.groovy
+++ netci.groovy
@@ -8,10 +8,11 @@ def platformList = ['Ubuntu16.04:Debian', 'Windows_2016:NanoServer']
 platformList.each { platform ->
     def(hostOS, containerOS) = platform.tokenize(':')
     def machineLabel = (hostOS == 'Windows_2016') ? 'latest-docker' : 'latest-or-auto-docker'
-    def versionList = (hostOS == 'Windows_2016') ? ['1.*', '2.0*', '2.1*'] : ['1.*', '2.*']
+    def versionList = (hostOS == 'Windows_2016') ? ['1.', '2.0', '2.1'] : ['1.', '2.']
 
     versionList.each { version ->
         def newJobName = Utilities.getFullJobName(project, "${containerOS}_${version}", isPR)
+        def versionFilter = "${version}*"
 
         def newJob = job(newJobName) {
             steps {
