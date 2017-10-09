4109388667d9e4bac8ebdd883c54e3435c93549c
Ravi Eda <raeda@microsoft.com>
2017-10-09 15:11:20 -0500

    Get git repository name.

--- scripts/Get-Tags.ps1
+++ scripts/Get-Tags.ps1
@@ -1,10 +1,26 @@
 param(
-    [string]$Branch="master",
-    [string]$ImageBuilderImageName="microsoft/dotnet-buildtools-prereqs:image-builder-jessie-20171005132855"
+    [string]$Branch='master',
+    [string]$ImageBuilderImageName='microsoft/dotnet-buildtools-prereqs:image-builder-jessie-20171005132855',
+    [string]$RepoName
 )
 
 $ErrorActionPreference = 'Stop'
 $repoRoot = Split-Path -Path "$PSScriptRoot" -Parent
+
+if ([String]::IsNullOrWhiteSpace($RepoName))
+{
+    [Uri]$remoteUrl = New-Object -TypeName System.Uri -ArgumentList "https://github.com/dotnet/dotnet-docker-nightly.git"
+    if ([Uri]::TryCreate(((git config --get remote.origin.url) | Out-String), [UriKind]::Absolute, [ref]$remoteUrl))
+    {
+        $RepoName = [System.IO.Path]::GetFileNameWithoutExtension($remoteUrl.ToString())
+    }
+}
+
+if ([String]::IsNullOrWhiteSpace($RepoName))
+{
+    Write-Warning 'Could not automatically determine repository name. Falling back to "dotnet-docker-nightly". Add -RepoName <REPO> to override.'
+    $RepoName = "dotnet-docker-nightly"
+}
 
 & docker pull $ImageBuilderImageName
 
@@ -13,5 +29,4 @@ $repoRoot = Split-Path -Path "$PSScriptRoot" -Parent
     -v "${repoRoot}:/repo" `
     -w /repo `
     $ImageBuilderImageName `
-    generateTagsReadme `
-    "https://github.com/dotnet/dotnet-docker-nightly/blob/${Branch}"
+    generateTagsReadme "https://github.com/dotnet/${RepoName}/blob/${Branch}"
