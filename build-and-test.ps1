dbfe01877d055803e7b49fecf5cf7e24ab86edf1
Ravi Eda <raeda@microsoft.com>
2018-01-22 11:25:17 -0600

    Cleanup Docker

--- build-and-test.ps1
+++ build-and-test.ps1
@@ -3,11 +3,28 @@ param(
     [switch]$UseImageCache,
     [string]$VersionFilter,
     [string]$ArchitectureFilter,
-    [string]$OSFilter
+    [string]$OSFilter,
+    [switch]$CleanupDocker
 )
 
 Set-StrictMode -Version Latest
 $ErrorActionPreference = 'Stop'
+
+function Invoke-CleanupDocker()
+{
+    if ($CleanupDocker)
+    {
+        docker ps -a -q | ForEach-Object { docker rm -f $_ }
+        docker images |
+            Where-Object { 
+                -Not ($_.StartsWith("microsoft/nanoserver ")`
+                -Or $_.StartsWith("microsoft/windowsservercore ")`
+                -Or $_.StartsWith("REPOSITORY ")) } |
+            ForEach-Object { $_.Split(' ', [System.StringSplitOptions]::RemoveEmptyEntries)[2] } |
+            Select-Object -Unique |
+            ForEach-Object { docker rmi -f $_ }
+    }
+}
 
 $(docker version) | % { Write-Host "$_" }
 
@@ -22,6 +39,7 @@ $manifest = Get-Content "manifest.json" | ConvertFrom-Json
 $manifestRepo = $manifest.Repos[0]
 $activeOS = docker version -f "{{ .Server.Os }}"
 $builtTags = @()
+Invoke-CleanupDocker
 
 $buildFilter = "*"
 if (-not [string]::IsNullOrEmpty($versionFilter))
@@ -60,5 +78,6 @@ $manifestRepo.Images |
     }
 
 ./test/run-test.ps1 -VersionFilter $VersionFilter -ArchitectureFilter $ArchitectureFilter -OSFilter $OSFilter
+Invoke-CleanupDocker
 
 Write-Host "Tags built and tested:`n$($builtTags | Out-String)"
